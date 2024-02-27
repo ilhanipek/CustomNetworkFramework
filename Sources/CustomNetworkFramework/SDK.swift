f239654c4da584d4645d15a833a26ab74f706c39
İlhan İpek <aliilhanipek92@gmail.com>
2024-02-27 21:46:21 +0300

    First commit of learning custom framework.

---
+++ Sources/CustomNetworkFramework/SDK.swift
@@ -0,0 +1,17 @@
+//
+//  SDK.swift
+//  Custom Framework Project
+//
+//  Created by ilhan serhan ipek on 27.02.2024.
+//
+
+import Foundation
+import SwiftUI
+
+struct SDK {
+
+  static func doSomething() {
+
+    print("Doing something..")
+  }
+}
