00360cce0a6289f606f1777e27f7f131da860af6
İlhan İpek <aliilhanipek92@gmail.com>
2024-03-03 23:52:34 +0300

    Practise2

---
+++ Sources/CustomNetworkFramework/HTTPMethod.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by ilhan serhan ipek on 3.03.2024.
+//
+
+import Foundation
---
+++ Sources/CustomNetworkFramework/Service.swift
@@ -0,0 +1,13 @@
+//
+//  File.swift
+//  
+//
+//  Created by ilhan serhan ipek on 3.03.2024.
+//
+
+import Foundation
+import SwiftUI
+
+open class VendorBase {
+  
+}
