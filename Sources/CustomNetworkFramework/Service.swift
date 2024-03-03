864c3c0c3c4eabf1a38d1309e4031543f9d24db5
İlhan İpek <aliilhanipek92@gmail.com>
2024-03-03 23:52:54 +0300

    Practise2

--- Sources/CustomNetworkFramework/Service.swift
+++ Sources/CustomNetworkFramework/Service.swift
@@ -9,5 +9,26 @@ import Foundation
 import SwiftUI
 
 open class VendorBase {
-  
+
+  class x {
+    var y : String
+
+    init(y: String) {
+      self.y = y
+    }
+
+    func sayHello() {
+      print(y)
+    }
+  }
 }
+
+func sayHi() {
+
+  VendorBase.x(y: "f").sayHello()
+}
+
+
+
+
+
