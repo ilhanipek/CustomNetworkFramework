d2540882c4a7bc152023c8f54538bf99882041c0
İlhan İpek <aliilhanipek92@gmail.com>
2024-03-26 12:59:47 +0300

    new

--- Sources/CustomNetworkFramework/VendorBase.swift
+++ Sources/CustomNetworkFramework/VendorBase.swift
@@ -13,12 +13,13 @@ public class Auth {
     var x = AuthEndPoint.customerSignIn
 
     public init() { }
-    
+
     open func login() {
       print("print \(x.baseURL)")
       print("print \(x.path)")
       print("print \(x.method)")
       print("print \(x.request())")
+      print("sdfksdfs")
     }
   }
 
